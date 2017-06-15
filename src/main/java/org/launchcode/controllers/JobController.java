bcbe97abfd398143ebac2461af0f8cd61d0eb608
Rebecca Fleak <fleakrl@gmail.com>
2017-06-15 18:41:14 -0500

    Single job is being successfully passed to view at /job?id=X

--- src/main/java/org/launchcode/controllers/JobController.java
+++ src/main/java/org/launchcode/controllers/JobController.java
@@ -1,5 +1,6 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.Job;
 import org.launchcode.models.forms.JobForm;
 import org.launchcode.models.data.JobData;
 import org.springframework.stereotype.Controller;
@@ -20,11 +21,12 @@ public class JobController {
     private JobData jobData = JobData.getInstance();
 
     // The detail display for a given Job at URLs like /job?id=17
-    @RequestMapping(value = "", method = RequestMethod.GET)
+    @RequestMapping( method = RequestMethod.GET)
     public String index(Model model, int id) {
 
         // TODO #1 - get the Job with the given ID and pass it into the view
-
+        JobData returnjob = JobData.getInstance();
+        model.addAttribute("job", returnjob.findById(id));
         return "job-detail";
     }
 
